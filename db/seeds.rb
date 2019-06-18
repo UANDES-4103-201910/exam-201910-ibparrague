1a1b2b6faa6e74f49b68679b39263c1f2d7e5edc
IgnacioParrague <ibparrague@miuandes.cl>
2019-06-18 12:02:29 -0400

    partially completed iteration 2, starting with part 3

--- db/seeds.rb
+++ db/seeds.rb
@@ -10,5 +10,7 @@ user1 = User.create(first_name: 'Luke', last_name: 'Skywalker',email:'firstmail@
 User.create(first_name: 'asd') #fails because of validations
 
 Product.create(kind: "handset",brand: 'paple', model:'I8', variant:'type-s',price: 200, short_description: 'newest phone from paple', long_description: 'the greatest product from the current line of paple devices will sweep you off your feets and roundhouse your FACE')
+Product.create(kind: "Service",brand: 'MEC', model:'E3', variant:'Papirus',price: 100, short_description: 'Lastest from MEC', long_description: 'The latest MEC is not only the best, but it blows the other products out of the water')
+
 
 Address.create(phone: 12345678, address_line_1: 'home1', address_line_2: 'home2', city:'City17', country:'black_mesa',zip_code:'test zipcode')
